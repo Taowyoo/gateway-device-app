be02767babe967084eb6653bc176bc7ac842faee
Yuxiang Cao <cao.yux@northeastern.edu>
2020-10-25 03:16:01 -0700

    refactor: Update data containers

--- src/main/java/programmingtheiot/data/BaseIotData.java
+++ src/main/java/programmingtheiot/data/BaseIotData.java
@@ -1,14 +1,15 @@
 /**
  * This class is part of the Programming the Internet of Things project.
- * 
+ * <p>
  * It is provided as a simple shell to guide the student and assist with
  * implementation for the Programming the Internet of Things exercises,
  * and designed to be modified by the student as needed.
- */ 
+ */
 
 package programmingtheiot.data;
 
 import java.io.Serializable;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -18,95 +19,104 @@ import programmingtheiot.common.ConfigConst;
  * Shell representation of class for student implementation.
  *
  */
-public abstract class BaseIotData implements Serializable
-{
-	// static
-	
-	public static final float DEFAULT_VAL = 0.0f;
-	public static final int DEFAULT_STATUS = 0;
-	
-	// private var's
+public abstract class BaseIotData implements Serializable {
+    // static
 
-    private String  name       = ConfigConst.NOT_SET;
-	private String  timeStamp  = null;
-    private boolean hasError   = false;
-    private int     statusCode = 0;
-    
-    private long    timeStampMillis = 0L;
+    public static final float DEFAULT_VAL = 0.0f;
+    public static final int DEFAULT_STATUS = 0;
 
-    
-	// constructors
-	
+    // private var's
+
+    private String name = ConfigConst.NOT_SET;
+    private String timeStamp = null;
+    private boolean hasError = false;
+    private int statusCode = DEFAULT_STATUS;
+
+
+    // constructors
+
+    /**
+     * Default Constructor
+     */
+    protected BaseIotData() {
+        super();
+        this.updateTimeStamp();
+    }
+
+
+    // public methods
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public long getTimeStampMillis() {
+        long timeStampMillis = 0L;
+        try {
+            timeStampMillis =  new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS").parse(this.timeStamp).getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return timeStampMillis;
+    }
+
+    public boolean hasErrorFlag() {
+        return hasError;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setStatusCode(int code) {
+        this.statusCode = code;
+    }
+
+    public void updateTimeStamp(){
+        Date date = new Date();
+        this.timeStamp = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS").format(date);
+    }
+
 	/**
-	 * Default.
-	 * 
+	 * Use given BaseIotData instance to update current instance
+	 * @param data Given BaseIotData instance (including subclass instance)
 	 */
-	protected BaseIotData()
-	{
-		super();
-	}
-	
-	
-	// public methods
-	
-	public String getName()
-	{
-		return null;
-	}
-	
-	public String getStateData()
-	{
-		return null;
-	}
-	
-	public int getStatusCode()
-	{
-		return 0;
-	}
-	
-	public String getTimeStamp()
-	{
-		return null;
-	}
-	
-	public long getTimeStampMillis()
-	{
-		return 0L;
-	}
-	
-	public boolean hasError()
-	{
-		return false;
-	}
-	
-	public void setName(String name)
-	{
-	}
-	
-	public void setStateData(String data)
-	{
-	}
-	
-	public void setStatusCode(int code)
-	{
-	}
-	
-	public void updateData(BaseIotData data)
-	{
-		// TODO: update local var's
-		
-		handleUpdateData(data);
-	}
-	
-	
-	// protected methods
-	
-	/**
-	 * Template method to handle data update for the sub-class.
-	 * 
-	 * @param BaseIotData While the parameter must implement this method,
-	 * the sub-class is expected to cast the base class to its given type.
-	 */
-	protected abstract void handleUpdateData(BaseIotData data);
-	
+	public void updateData(BaseIotData data) {
+        // update local super class variables
+        this.name = data.name;
+        this.timeStamp = data.timeStamp;
+        this.hasError = data.hasError;
+        this.statusCode = data.statusCode;
+        // update subclass variables
+        handleUpdateData(data);
+    }
+
+    @Override
+    public String toString() {
+        return "BaseIotData{" +
+                "name='" + name + '\'' +
+                ", timeStamp='" + timeStamp + '\'' +
+                ", hasError=" + hasError +
+                ", statusCode=" + statusCode +
+                '}';
+    }
+
+    // protected methods
+
+    /**
+     * Template method to handle data update for the sub-class.
+     *
+     * @param data While the parameter must implement this method,
+     * the sub-class is expected to cast the base class to its given type.
+     */
+    protected abstract void handleUpdateData(BaseIotData data);
+
 }
--- src/main/java/programmingtheiot/data/SensorData.java
+++ src/main/java/programmingtheiot/data/SensorData.java
@@ -9,6 +9,8 @@
 package programmingtheiot.data;
 
 import java.io.Serializable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Shell representation of class for student implementation.
@@ -17,15 +19,15 @@ import java.io.Serializable;
 public class SensorData extends BaseIotData implements Serializable
 {
 	// static
-	
+	private static final Logger _Logger = Logger.getLogger(SystemStateData.class.getName());
+
 	public static final int DEFAULT_SENSOR_TYPE = 0;
-	
 
 	// private var's
-	
-    
+	private int sensorType = DEFAULT_SENSOR_TYPE;
+    private  float value = DEFAULT_VAL;
+
 	// constructors
-	
 	public SensorData()
 	{
 		super();
@@ -33,22 +35,39 @@ public class SensorData extends BaseIotData implements Serializable
 	
 	public SensorData(int sensorType)
 	{
-		super();
+		this();
+		this.sensorType = sensorType;
 	}
 	
 	
 	// public methods
-	
+
 	public float getValue()
 	{
-		return 0.0f;
+		return value;
 	}
 	
 	public void setValue(float val)
 	{
+		this.value = val;
 	}
-	
-	
+
+	public int getSensorType() {
+		return sensorType;
+	}
+
+	public void setSensorType(int sensorType) {
+		this.sensorType = sensorType;
+	}
+
+	@Override
+	public String toString() {
+		return "SensorData{" +
+				"sensorType=" + sensorType +
+				", value=" + value +
+				"} " + super.toString();
+	}
+
 	// protected methods
 	
 	/* (non-Javadoc)
@@ -56,6 +75,13 @@ public class SensorData extends BaseIotData implements Serializable
 	 */
 	protected void handleUpdateData(BaseIotData data)
 	{
+		if (data instanceof SensorData){
+			SensorData sensorData = (SensorData)data;
+			this.sensorType = sensorData.sensorType;
+			this.value = sensorData.value;
+		}
+		else{
+			_Logger.log(Level.WARNING, "Got invalid data when handleUpdateData!");
+		}
 	}
-	
 }

da17d7a704a9a3cffe7ea7a75076a1e658c4e57e
Yuxiang Cao <cao.yux@northeastern.edu>
2020-12-12 17:37:11 -0800

    feat: Add auto logging to file

--- src/main/java/programmingtheiot/gda/app/GatewayDeviceApp.java
+++ src/main/java/programmingtheiot/gda/app/GatewayDeviceApp.java
@@ -15,8 +15,11 @@ import programmingtheiot.common.ConfigConst;
 import programmingtheiot.common.ConfigUtil;
 import programmingtheiot.gda.system.SystemPerformanceManager;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 /**
  * Main GDA application.
@@ -60,6 +63,22 @@ public class GatewayDeviceApp
 	 */
 	public static void main(String[] args)
 	{
+		// setup logging
+		Logger logger = Logger.getLogger("");
+		FileHandler fh;
+		try {
+			// This block configure the logger with handler and formatter
+			fh = new FileHandler("logs/LogFile.log");
+			logger.addHandler(fh);
+			SimpleFormatter formatter = new SimpleFormatter();
+			fh.setFormatter(formatter);
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// start the app
 		GatewayDeviceApp gwApp = new GatewayDeviceApp(args);
 		
 		gwApp.startApp();
